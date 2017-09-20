298c70d5decc1abff7475b09b3d8cb765a22008b
tech <n1000ss@yahoo.co.jp>
2017-09-20 12:15:50 +0000

    add colum

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
     @tasks = Task.all
   end
   
   def show
-    @task = Task.find(params[:id])
   end
   
   def new
@@ -24,12 +25,9 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = Task.find(params[:id])
   end
   
   def update
-    @task = Task.find(params[:id])
-    
     if @task.update(task_params)
       flash[:success] = "タスクは正常に更新されました"
       redirect_to @task
@@ -37,10 +35,9 @@ class TasksController < ApplicationController
       flash.now[:danger] = "タスクは更新されませんでした"
       render :edit
     end
-  end  
+  end
   
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     
     flash[:success] = "タスクは正常に削除されました"
@@ -49,8 +46,12 @@ class TasksController < ApplicationController
   
   private
   
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
   # Strong Parameter
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 end
